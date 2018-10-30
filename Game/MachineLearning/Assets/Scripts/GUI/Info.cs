c48f4d11688a246c69e9a16b65e43f326fcd92f4
Wesley Cats <23973@ma-web.nl>
2018-10-30 12:35:54 +0100

    Added machine learning: follow player demo

---
+++ Game/MachineLearning/Assets/Scripts/GUI/Info.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info : MonoBehaviour {
+
+	public Text generation;
+	public Text topFitness;
+	public Text timer;
+	public GameManager gameManager;
+	public List<float> allFitness = null;
+
+	void Start () {
+		allFitness = new List<float>();
+	}
+	
+	void Update () {
+		generation.text = "GENERATION: " + gameManager.generationNumber;
+		
+		// Itterates over all enemys and adds their fitness to the list
+		for (int i = 0; i < gameManager.enemyList.Count; i++)
+		{
+			allFitness.Add(gameManager.enemyList[i].net.fitness);
+		}
+		
+		if (allFitness.Count > 0)
+		{
+			allFitness.Sort();
+			topFitness.text = "TOP FITNESS: " + allFitness[allFitness.Count - 1];
+		}
+
+		timer.text = "TIMER: " + (Mathf.Round(gameManager.timer * 10f) / 10f) + " seconds";
+	}
+}
